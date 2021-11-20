604c707c061c62a057715cdd8b99bd9dcafb1dd6
vladyslavnUA <vladyslavn@yahoo.com>
2021-11-19 22:03:55 -0500

    added database and first route

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
 const exphbs = require('express-handlebars');
+const mongoose = require("mongoose");
 
+// configs 
 var hbs = exphbs.create({
     defaultLayout: 'main',
 });
@@ -9,8 +11,35 @@ var hbs = exphbs.create({
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// db connection
+
+const mongoURI = "mongodb+srv://test-user:test123@tomatoes.thcnz.mongodb.net/test"
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "Mongodb connect error: "));
+
+// schema
+const Review = mongoose.model("Review", {
+    title: String,
+    movieTitle: String,
+});
+
+// sample data
+
+// let reviews = [
+//     { title: "Great Review", movieTitle: "Batman II" },
+//     { title: "Awesome Movie", movieTitle: "Titanic" }
+// ]
+
+// functions 
+
 app.get('/', (req, res) => {
-  res.render('home')
+    Review.find().lean().then(reviews => {
+        res.render('reviews-index', { reviews });
+    })
+    .catch(err => {
+        console.log('err: ',err);
+    })
 })
 
 app.listen(3000, () => {
